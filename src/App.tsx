02a75c26de3c334e1d92bdda9e4d4064e9ee16d2
masum <md.wr.masum@gmail.com>
2021-07-12 10:15:14 +0600

    complete react saample project

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Body from "./components/layout/body.component";
+import Header from "./components/layout/header.component";
+
+// import MonsterMain from "./components/monster/monster-main";
+// import CascadingDropdown from "./components/conditional-render/cascading-dropdown.component";
+// import Hooks from "./components/hooks/hooks.component";
+// import JsonData from "./components/json/json-data.component";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Header />
+      <Body />
+    </React.Fragment>
+    // <div className="App">
+    //  <MonsterMain />
+    //  {/* <JsonData/> */}
+    //  {/* <Hooks /> */}
+    //  <CascadingDropdown/>
+    // </div>
   );
 }
 
